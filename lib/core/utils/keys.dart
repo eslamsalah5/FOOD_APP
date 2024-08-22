f0bd9d7a27d12dae13f34f6f4cc885a620e3163f
Eslam salah <113712183+eslamsalah5@users.noreply.github.com>
2024-08-22 10:30:34 +0300

    Update keys.dart

--- lib/core/utils/keys.dart
+++ lib/core/utils/keys.dart
@@ -1,11 +1,11 @@
 class Keys {
   static String publishableKey =
-      "pk_test_51PoyQxDfmNuhtiwAYPxPz8jfnnK62wXX7XVaRLFV7iOiXz3cd9jCcZGU8RtOkV06lE1hwImoXWjB3wQjU18k96ce00ATbgmVOM";
+      "";
 
   static String secretKey =
-      "sk_test_51PoyQxDfmNuhtiwAZlITpBYoo0bzq1xRaCTTAnc7AGmIisjgC8cUSb3yvP7P0QB5eWzzfdoKvmhXUo3QRtD1BSsP00hNFN5cXI";
+      "";
 
-  static String clientId = "AfqfFwgcfJtov6-zgZRkcUQarWPpi5pSXqGQ5u3g92x_FU4guXrujLSGO6YevYBz9L3Om5sa1UXDGbjS";
+  static String clientId = "";
 
-  static String secretKeyPaypal = "ELYzB4Ro4J-9QQb2h6hHirPbRl74ems-O5Sbs1BKcEzaTPzrwYDBYge6wkjB6j8NpzAd2fh-bmWmTdb7";
+  static String secretKeyPaypal = "";
 }
